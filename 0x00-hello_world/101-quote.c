a68867f7722467e3bd26bd0fdc8ca7479f58826f
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-06-30 22:09:31 -0700

    guote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - prints exactly the stated quote
+ *
+ * followed by a new line
+ *
+ * return: always 0(success)
+ * /
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
