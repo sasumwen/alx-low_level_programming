04a3eb5977661d15ecbf7f4518d725733b35589e
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-07-01 19:23:00 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,20 +2,21 @@
 
 /**
  * main -> print single digit numbers of base 10
- * folowed by a new line
- * return/: always 0
+ * followed by a new line
+ * Return: always 0
  */
 
 int main(void)
 {
 	int ch;
 
-	for (ch = 49; ch <= 57; ch++)
+	for (ch = 48; ch <= 57; ch++)
 	{
 		putchar(ch);
 	}
+
 	putchar(10);
 
-	retutn (0);
+	return (0);
 }
-	
+
