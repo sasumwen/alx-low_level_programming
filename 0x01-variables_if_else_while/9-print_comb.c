dca98a1e9db63d54bf026141d36021ce2a2ff6de
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-07-01 19:48:59 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -> program to prin all combo of 
+ * single digit numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
