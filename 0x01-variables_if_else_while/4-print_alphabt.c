4b71c4e0a0fe8ffb92aa89d4be0ca93ddd60d033
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-07-01 18:39:46 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main ->program that prints the alphabet in lowercase
+ * main -> program that prints the alphabet in lowercase
  * followed by a new line
  * Return : always 0
 */
 
-int main(void /* program starts*/)
+int main(void)
 {
 	char ch;
 
