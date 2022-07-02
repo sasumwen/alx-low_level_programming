9a06383c830dd376467a6b1aa194226baedd8e60
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-07-01 17:33:22 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("%d and is 0", n % 10);
 	}
-	if (n % 10 < 6 && n%10 != 0)
+	if (n % 10 < 6 && n % 10 != 0)
 	{
 		printf("%d and is less than 6 and not 0", n % 10);
 	}
