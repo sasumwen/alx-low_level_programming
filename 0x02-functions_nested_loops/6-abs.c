79f51f230d287fc38e4d6263451c37c22dd4d224
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-12-14 13:41:09 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs -> return absolutre value of a number
+ * @n: param
+ * Return: abs value of num
+ */
+
+int_abs(int n);
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
