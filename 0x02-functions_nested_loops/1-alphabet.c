4fb4685e3a8ca97fade23f0009a7b34ef9dd9246
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-12-13 20:21:55 -0800

    betty fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet
+ * print_alphabet -prints the given
  * return always 0
  */
 
 void print_alphabet(void)
+
 {
 	int j;
+
 	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar(j);
