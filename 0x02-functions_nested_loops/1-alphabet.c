3f475bb458ba798c27bea7c722baa31ad4a1af1f
sasumwen <imarhiagbeosasumwen@gmail.com>
2022-12-13 20:16:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet
+ * return always 0
+ */
+
+void print_alphabet(void)
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
